6e7d42a323e3193be48136b3e2dde50536684ac1
Cristian.wil88 <Cristian.wil88@203a1259-2390-5e5b-c600-290653cded0a>
2010-09-19 05:35:46 +0000

    New Feature, Symbol Table!

---
+++ trunk/rlw-compiler-2010/src/rlwcompiler2010/SymbolData.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package rlwcompiler2010;
+
+/**
+ *
+ * @author je
+ */
+class SymbolData {
+
+}
--- trunk/rlw-compiler-2010/src/rlwcompiler2010/Token.java
+++ trunk/rlw-compiler-2010/src/rlwcompiler2010/Token.java
@@ -2,8 +2,8 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package rlwcompiler2010;
+
 import java.util.HashMap;
 
 /**
@@ -13,20 +13,28 @@ import java.util.HashMap;
 public class Token {
 
     public enum Tokens {
-        ID,CONST_INT,CONST_REAL,KEYWORD,
-        SUM,SUB,MULT,DIV, //Arithmetic Operators
-        GT_EQUAL,LT_EQUAL,GT,LT, EQUAL, DISTINCT, //RelationShip Operators
-        COMMENT,SEMICOLON,COLON,O_PARENT,C_PARENT,O_BRACKET,C_BRACKET,
-        ASIGN,TEXT}
 
+        ID, CONST_INT, CONST_REAL, KEYWORD,
+        SUM, SUB, MULT, DIV, //Arithmetic Operators
+        GT_EQUAL, LT_EQUAL, GT, LT, EQUAL, DISTINCT, //RelationShip Operators
+        COMMENT, SEMICOLON, COLON, O_PARENT, C_PARENT, O_BRACKET, C_BRACKET,
+        ASIGN, TEXT
+    }
     private Tokens token;
     private String symbol;
-    private HashMap<Tokens, String> dicc = new HashMap<Tokens, String>();
+    private static HashMap<Tokens, String> dicc = null;
+
+    public boolean matches(String s) {
+        return symbol.equals(s);
+    }
 
     public String get() {
         return dicc.get(token);
     }
-    
+    public Tokens getID() {
+        return token;
+    }
+
     public String getString() {
         return symbol;
     }
@@ -34,32 +42,36 @@ public class Token {
     public Token(String s, Tokens t) {
         symbol = s.trim();
         token = t;
-        dicc.put(Tokens.ID, "ID");
-        dicc.put(Tokens.CONST_INT, "CONST_INT");
-        dicc.put(Tokens.CONST_REAL, "CONST_REAL");
-        dicc.put(Tokens.KEYWORD, "KEYWORD");
+        if (dicc == null) {
+            dicc = new HashMap<Tokens, String>();
+            dicc.put(Tokens.ID, "ID");
+            dicc.put(Tokens.CONST_INT, "CONST_INT");
+            dicc.put(Tokens.CONST_REAL, "CONST_REAL");
+            dicc.put(Tokens.KEYWORD, "KEYWORD");
 
-        dicc.put(Tokens.SUM, "SUM");
-        dicc.put(Tokens.SUB, "SUB");
-        dicc.put(Tokens.MULT, "MULT");
-        dicc.put(Tokens.DIV, "DIV");
+            dicc.put(Tokens.SUM, "SUM");
+            dicc.put(Tokens.SUB, "SUB");
+            dicc.put(Tokens.MULT, "MULT");
+            dicc.put(Tokens.DIV, "DIV");
 
-        dicc.put(Tokens.GT_EQUAL, "GT_EQUAL");
-        dicc.put(Tokens.LT_EQUAL, "LT_EQUAL");
-        dicc.put(Tokens.GT, "GT");
-        dicc.put(Tokens.LT, "LT");
-        dicc.put(Tokens.EQUAL, "EQUAL");
-        dicc.put(Tokens.DISTINCT, "DISTINCT");
+            dicc.put(Tokens.GT_EQUAL, "GT_EQUAL");
+            dicc.put(Tokens.LT_EQUAL, "LT_EQUAL");
+            dicc.put(Tokens.GT, "GT");
+            dicc.put(Tokens.LT, "LT");
+            dicc.put(Tokens.EQUAL, "EQUAL");
+            dicc.put(Tokens.DISTINCT, "DISTINCT");
 
-        dicc.put(Tokens.COMMENT, "COMMENT");
-        dicc.put(Tokens.SEMICOLON, "SEMICOLON");
-        dicc.put(Tokens.COLON, "COLON");
-        dicc.put(Tokens.O_PARENT, "O_PARENT");
-        dicc.put(Tokens.C_PARENT, "C_PARENT");
-        dicc.put(Tokens.O_BRACKET, "O_BRACKET");
-        dicc.put(Tokens.C_BRACKET, "C_BRACKET");
+            dicc.put(Tokens.COMMENT, "COMMENT");
+            dicc.put(Tokens.SEMICOLON, "SEMICOLON");
+            dicc.put(Tokens.COLON, "COLON");
+            dicc.put(Tokens.O_PARENT, "O_PARENT");
+            dicc.put(Tokens.C_PARENT, "C_PARENT");
+            dicc.put(Tokens.O_BRACKET, "O_BRACKET");
+            dicc.put(Tokens.C_BRACKET, "C_BRACKET");
 
-        dicc.put(Tokens.ASIGN, "ASIGN");
-        dicc.put(Tokens.TEXT, "TEXT");
+            dicc.put(Tokens.ASIGN, "ASIGN");
+            dicc.put(Tokens.TEXT, "TEXT");
+
+        }
     }
 }
